2477c9e86434cfc5bf0ddc9163984a59363a6c92
Honza Kral <honza.kral@gmail.com>
2012-12-03 22:57:51 +0100

    South migrations

---
+++ ella_quizzes/migrations/0001_initial.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+    depends_on = (
+        ("core", "0002_initial_publishable"),
+    )
+
+    def forwards(self, orm):
+        # Adding model 'Quiz'
+        db.create_table('ella_quizzes_quiz', (
+            ('publishable_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['core.Publishable'], unique=True, primary_key=True)),
+            ('intro_title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('intro_text', self.gf('django.db.models.fields.TextField')()),
+            ('choices', self.gf('django.db.models.fields.PositiveIntegerField')()),
+        ))
+        db.send_create_signal('ella_quizzes', ['Quiz'])
+
+        # Adding model 'Result'
+        db.create_table('ella_quizzes_result', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('quiz', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ella_quizzes.Quiz'])),
+            ('choice', self.gf('django.db.models.fields.IntegerField')()),
+            ('photo', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['photos.Photo'], null=True, on_delete=models.SET_NULL, blank=True)),
+            ('text', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('ella_quizzes', ['Result'])
+
+        # Adding unique constraint on 'Result', fields ['quiz', 'choice']
+        db.create_unique('ella_quizzes_result', ['quiz_id', 'choice'])
+
+        # Adding model 'Question'
+        db.create_table('ella_quizzes_question', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('quiz', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ella_quizzes.Quiz'])),
+            ('order', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('photo', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['photos.Photo'], null=True, on_delete=models.SET_NULL, blank=True)),
+            ('text', self.gf('django.db.models.fields.TextField')()),
+            ('choices_data', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('ella_quizzes', ['Question'])
+
+        # Adding unique constraint on 'Question', fields ['quiz', 'order']
+        db.create_unique('ella_quizzes_question', ['quiz_id', 'order'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Question', fields ['quiz', 'order']
+        db.delete_unique('ella_quizzes_question', ['quiz_id', 'order'])
+
+        # Removing unique constraint on 'Result', fields ['quiz', 'choice']
+        db.delete_unique('ella_quizzes_result', ['quiz_id', 'choice'])
+
+        # Deleting model 'Quiz'
+        db.delete_table('ella_quizzes_quiz')
+
+        # Deleting model 'Result'
+        db.delete_table('ella_quizzes_result')
+
+        # Deleting model 'Question'
+        db.delete_table('ella_quizzes_question')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.author': {
+            'Meta': {'object_name': 'Author'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['photos.Photo']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'core.category': {
+            'Meta': {'unique_together': "(('site', 'tree_path'),)", 'object_name': 'Category'},
+            'app_data': ('app_data.fields.AppDataField', [], {'default': "'{}'"}),
+            'content': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'template': ('django.db.models.fields.CharField', [], {'default': "'category.html'", 'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'tree_parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']", 'null': 'True', 'blank': 'True'}),
+            'tree_path': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'core.publishable': {
+            'Meta': {'object_name': 'Publishable'},
+            'announced': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'app_data': ('app_data.fields.AppDataField', [], {'default': "'{}'"}),
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Author']", 'symmetrical': 'False'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']"}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['photos.Photo']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'publish_from': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(3000, 1, 1, 0, 0, 0, 2)', 'db_index': 'True'}),
+            'publish_to': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Source']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'static': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'core.source': {
+            'Meta': {'object_name': 'Source'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'ella_quizzes.question': {
+            'Meta': {'unique_together': "(('quiz', 'order'),)", 'object_name': 'Question'},
+            'choices_data': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['photos.Photo']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'quiz': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ella_quizzes.Quiz']"}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        'ella_quizzes.quiz': {
+            'Meta': {'object_name': 'Quiz', '_ormbases': ['core.Publishable']},
+            'choices': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'intro_text': ('django.db.models.fields.TextField', [], {}),
+            'intro_title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'publishable_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.Publishable']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'ella_quizzes.result': {
+            'Meta': {'unique_together': "(('quiz', 'choice'),)", 'object_name': 'Result'},
+            'choice': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['photos.Photo']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'quiz': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ella_quizzes.Quiz']"}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        'photos.photo': {
+            'Meta': {'object_name': 'Photo'},
+            'app_data': ('app_data.fields.AppDataField', [], {'default': "'{}'"}),
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'photo_set'", 'symmetrical': 'False', 'to': "orm['core.Author']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'height': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
+            'important_bottom': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_left': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_right': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_top': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Source']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'width': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['ella_quizzes']
