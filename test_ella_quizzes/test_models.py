664219539323a2e740a098beb90ae32ffd9042d5
Honza Kral <honza.kral@gmail.com>
2012-12-03 22:16:12 +0100

    basic quiz tests

--- test_ella_quizzes/test_models.py
+++ test_ella_quizzes/test_models.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
-from ella_quizzes.models import Question, Quiz
+from ella.utils.test_helpers import create_basic_categories
+from ella.utils.timezone import now
+
+from ella_quizzes.models import Question, Quiz, Result
 
 from nose import tools
 
@@ -37,3 +40,38 @@ class TestQuestion(TestCase):
         q = Question(quiz=quiz)
         q.choices = ['just', 'right']
         q.full_clean(exclude=['quiz', 'order', 'text'])
+
+class TestQuiz(TestCase):
+    def setUp(self):
+        super(TestQuiz, self).setUp()
+        create_basic_categories(self)
+        self.quiz = Quiz.objects.create(
+                title=u'First Quiz',
+                slug=u'first-quiz',
+                description=u'First quiz',
+                category=self.category_nested,
+                publish_from=now(),
+                published=True,
+
+                intro_title='Answer me!',
+                intro_text='Some even longer test. \n' * 5,
+                choices=3
+        )
+        self.results = [
+            Result.objects.create(
+                quiz=self.quiz,
+                choice=ch,
+                text='You chose %d' % ch
+            ) for ch in range(3)
+        ]
+        self.questions = [
+            Question.objects.create(
+                quiz=self.quiz,
+                order=x,
+                text='WTF %d?!' % x,
+                choices_data=Question.SEPARATOR.join('%d FTW!' % ch for ch in range(3))
+            ) for x in range(5)
+        ]
+
+    def test_get_returns_result_for_top_choice(self):
+        tools.assert_equals(self.results[1], self.quiz.get_result(['0', '1', '2', '1', '1']))
