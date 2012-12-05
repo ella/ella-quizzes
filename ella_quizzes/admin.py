130cc2c381dbe413689070ea07815a998f3d0a21
andy <andy@whiskeymedia.com>
2012-12-05 15:59:18 -0800

    raw_id_fields for photos

--- ella_quizzes/admin.py
+++ ella_quizzes/admin.py
@@ -6,11 +6,14 @@ from ella_quizzes.forms import ResultInlineFormset
 class ResultInline(admin.TabularInline):
     model = Result
     formset = ResultInlineFormset
+    raw_id_fields = ('photo',)
 
 class QuestionInline(admin.TabularInline):
     model = Question
+    raw_id_fields = ('photo',)
 
 class QuizModelAdmin(admin.ModelAdmin):
     inlines = [ResultInline, QuestionInline]
+    raw_id_fields = ('photo',)
 
 admin.site.register(Quiz, QuizModelAdmin)
